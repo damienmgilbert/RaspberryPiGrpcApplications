5a594630f98f10ba04a1a7db1f95c4e50282929e
Damien <damienmgilbert@gmail.com>
2023-11-25 12:20:40 -0600

    Finally got a working grpc api!

--- RaspberryPi4GpioServiceLibrary/GpioService.cs
+++ RaspberryPi4GpioServiceLibrary/GpioService.cs
@@ -4,8 +4,13 @@ using System.Device.Gpio;
 namespace RaspberryPi4GpioServiceLibrary;
 public class GpioService
 {
-    readonly ILogger<GpioService> logger;
+
+    #region Fields
     readonly GpioController gpioController;
+    readonly ILogger<GpioService> logger;
+    #endregion
+
+    #region Constructors
     public GpioService(ILogger<GpioService> logger)
     {
         this.logger = logger;
@@ -21,6 +26,172 @@ public class GpioService
             }
         }
     }
+    #endregion
 
+    #region Public methods
+    public bool ClosePin(int pinNumber)
+    {
+        bool result;
+        try
+        {
+            this.gpioController.ClosePin(pinNumber);
+            result = true;
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Could not close pin");
+            result = false;
+        }
+        return result;
+    }
+    public string GetNumberingScheme() => this.gpioController.NumberingScheme.ToString();
+    public int GetPinCount() => this.gpioController.PinCount;
+    public string GetPinMode(int pinNumber) => this.gpioController.GetPinMode(pinNumber).ToString();
+    public bool IsPinModeSupported(int pinNumber, string pinMode)
+    {
+        bool result = false;
+        try
+        {
+            switch (pinMode)
+            {
+                case "InputPullUp":
+                    {
+                        result = this.gpioController.IsPinModeSupported(pinNumber, PinMode.InputPullUp);
+                        break;
+                    }
+                case "InputPullDown":
+                    {
+                        result = this.gpioController.IsPinModeSupported(pinNumber, PinMode.InputPullDown);
+                        break;
+                    }
+                case "Input":
+                    {
+                        result = this.gpioController.IsPinModeSupported(pinNumber, PinMode.Input);
+                        break;
+                    }
+                case "Output":
+                    {
+                        result = this.gpioController.IsPinModeSupported(pinNumber, PinMode.Output);
+                        break;
+                    }
+                default:
+                    {
+                        throw new NotImplementedException("Pin mode is not supported.");
+                    }
+            }
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Something went wrong when trying to figure out if pin mode was supported.");
+            result = false;
+        }
+        return result;
+    }
     public bool IsPinOpen(int pinNumber) => this.gpioController.IsPinOpen(pinNumber);
+    public bool OpenPin(int pinNumber)
+    {
+        bool result;
+        try
+        {
+            this.gpioController.OpenPin(pinNumber);
+            result = true;
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Could not open pin");
+            result = false;
+        }
+        return result;
+    }
+    public string Read(int pinNumber)
+    {
+        string result = string.Empty;
+        try
+        {
+            result = this.gpioController.Read(pinNumber).ToString();
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Unable to read value from pin.");
+        }
+        return result;
+    }
+    public bool SetPinMode(int pinNumber, string pinMode)
+    {
+        bool result = false;
+        try
+        {
+            switch (pinMode)
+            {
+                case "InputPullUp":
+                    {
+                        this.gpioController.SetPinMode(pinNumber, PinMode.InputPullUp);
+                        result = true;
+                        break;
+                    }
+                case "InputPullDown":
+                    {
+                        this.gpioController.SetPinMode(pinNumber, PinMode.InputPullDown);
+                        result = true;
+                        break;
+                    }
+                case "Input":
+                    {
+                        this.gpioController.SetPinMode(pinNumber, PinMode.Input);
+                        result = true;
+                        break;
+                    }
+                case "Output":
+                    {
+                        this.gpioController.SetPinMode(pinNumber, PinMode.Output);
+                        result = true;
+                        break;
+                    }
+                default:
+                    {
+                        throw new NotImplementedException("Pin mode is not supported.");
+                    }
+            }
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Could not set pin mode.");
+            result = false;
+        }
+        return result;
+    }
+    public bool Write(int pinNumber, string value)
+    {
+        bool result;
+        try
+        {
+            switch (value)
+            {
+                case "HIGH":
+                    {
+                        this.gpioController.Write(pinNumber, PinValue.High);
+                        result = true;
+                        break;
+                    }
+                case "LOW":
+                    {
+                        this.gpioController.Write(pinNumber, PinValue.Low);
+                        result = true;
+                        break;
+                    }
+                default:
+                    {
+                        throw new NotImplementedException("Pin value is not supported");
+                    }
+            }
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Pin value is not supported");
+            result = false;
+        }
+        return result;
+    }
+    #endregion
+
 }
--- Rpi4GpioGrpcService/Services/CommanderService.cs
+++ Rpi4GpioGrpcService/Services/CommanderService.cs
@@ -5,14 +5,53 @@ using Rpi4GpioGrpcService;
 namespace Rpi4GpioGrpcService.Services;
 public class CommanderService : Commander.CommanderBase
 {
-    private readonly ILogger<CommanderService> logger;
+
+    #region Fields
     private readonly GpioService gpioService;
+    private readonly ILogger<CommanderService> logger;
+    #endregion
+
+    #region Constructors
     public CommanderService(ILogger<CommanderService> logger, GpioService gpioService)
     {
         this.logger = logger;
         this.gpioService = gpioService;
-    }
 
+    }
+    #endregion
+
+    #region Public methods
+    public override async Task<ClosePinReply> ClosePin(ClosePinRequest request, ServerCallContext context)
+    {
+        bool result = this.gpioService.ClosePin(request.PinNumber);
+        return await Task.FromResult(new ClosePinReply
+        {
+            IsPinClosed = result
+        });
+    }
+    public override async Task<GetNumberingSchemeReply> GetNumberingScheme(GetNumberingSchemeRequest request, ServerCallContext context)
+    {
+        var numberingScheme = this.gpioService.GetNumberingScheme();
+        return await Task.FromResult(new GetNumberingSchemeReply { NumberingScheme = numberingScheme });
+    }
+    public override async Task<GetPinCountReply> GetPinCount(GetPinCountRequest request, ServerCallContext context)
+    {
+        var pinCount = this.gpioService.GetPinCount();
+        return await Task.FromResult(new GetPinCountReply { PinCount = pinCount });
+    }
+    public override async Task<GetPinModeReply> GetPinMode(GetPinModeRequest request, ServerCallContext context)
+    {
+        string pinMode = this.gpioService.GetPinMode(request.PinNumber);
+        return await Task.FromResult(new GetPinModeReply
+        {
+            PinMode = pinMode
+        });
+    }
+    public override async Task<IsPinModeSupportedReply> IsPinModeSupported(IsPinModeSupportedRequest request, ServerCallContext context)
+    {
+        var isPinModeSupported = this.gpioService.IsPinModeSupported(request.PinNumber, request.PinMode);
+        return await Task.FromResult(new IsPinModeSupportedReply { IsPinModeSupported = isPinModeSupported });
+    }
     public override async Task<IsPinOpenReply> IsPinOpen(IsPinOpenRequest request, ServerCallContext context)
     {
         bool result = this.gpioService.IsPinOpen(request.PinNumber);
@@ -21,4 +60,29 @@ public class CommanderService : Commander.CommanderBase
             IsPinOpen = result
         });
     }
+    public override async Task<OpenPinReply> OpenPin(OpenPinRequest request, ServerCallContext context)
+    {
+        bool result = this.gpioService.OpenPin(request.PinNumber);
+        return await Task.FromResult(new OpenPinReply
+        {
+            IsPinOpen = result
+        });
+    }
+    public override async Task<ReadReply> Read(ReadRequest request, ServerCallContext context)
+    {
+        var pinValue = this.gpioService.Read(request.PinNumber);
+        return await Task.FromResult(new ReadReply { PinValue = pinValue });
+    }
+    public override async Task<SetPinModeReply> SetPinMode(SetPinModeRequest request, ServerCallContext context)
+    {
+        var isSet = this.gpioService.SetPinMode(request.PinNumber, request.PinMode);
+        return await Task.FromResult(new SetPinModeReply { IsSet = isSet });
+    }
+    public override async Task<WriteReply> Write(WriteRequest request, ServerCallContext context)
+    {
+        var didWrite = this.gpioService.Write(request.PinNumber, request.PinValue);
+        return await Task.FromResult(new WriteReply { DidWrite = didWrite });
+    }
+    #endregion
+
 }
